fc4dc2559ff2c3674b374bcb2a06729786a22da1
Alice Cieslik <alice.cieslik@gmail.com>
2022-06-15 16:04:01 +0200

    Add and setup prettier

--- js/login.js
+++ js/login.js
@@ -2,12 +2,12 @@ const loginButton = document.querySelector('[data-js="button-login"]');
 
 let isUserLoggedIn = false;
 
-loginButton.addEventListener("click", () => {
+loginButton.addEventListener('click', () => {
   isUserLoggedIn = !isUserLoggedIn;
   if (isUserLoggedIn) {
-    loginButton.innerText = "Logout";
+    loginButton.innerText = 'Logout';
     console.log(isUserLoggedIn);
   } else {
-    loginButton.innerText = "Login";
+    loginButton.innerText = 'Login';
   }
 });
