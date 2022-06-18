4a165b9a169cf40d761691e0273887fa23c966a4
Alice Cieslik <alice.cieslik@gmail.com>
2022-06-18 18:58:33 +0200

    change innerText of answer button by click

--- js/answer.js
+++ js/answer.js
@@ -1,12 +1,17 @@
 export default function toggleAnswer(){
-  const cards = document.querySelectorAll("[data-js=card]")
+  const cards = document.querySelectorAll("[data-js=card]");
   cards.forEach((card) => {
       const answerButton = card.querySelector("[data-js=button]");
       const answer = card.querySelector("[data-js=answer]");
+      let answerHidden = true;
           answerButton.addEventListener("click", () => {
+            answerHidden = !answerHidden;
+            if (answerHidden) {
+              answerButton.innerText = 'Show Answer';
+            } else {
+              answerButton.innerText = 'Hide Answer'
+            }
               answer.classList.toggle("hidden")
           })
   })
 }
-
-// export { toggleAnswer };
